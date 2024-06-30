fb7000c7b48731c8f937d2280c2a5d1c3aa84d13
Zkaria Gamal <115581685+zkzkGamal@users.noreply.github.com>
2024-06-30 16:05:29 +0300

    Update views.py

--- blog_api/views.py
+++ blog_api/views.py
@@ -32,47 +32,6 @@ class PostList(viewsets.ModelViewSet):
         return Post.objects.all()
 
 
-# class PostList(viewsets.ViewSet):
-#     permission_classes = [IsAuthenticated]
-#     queryset = Post.postobjects.all()
-
-#     def list(self, request):
-#         serializer_class = PostSerializer(self.queryset, many=True)
-#         return Response(serializer_class.data)
-
-#     def retrieve(self, request, pk=None):
-#         post = get_object_or_404(self.queryset, pk=pk)
-#         serializer_class = PostSerializer(post)
-#         return Response(serializer_class.data)
-
-    # def list(self, request):
-    #     pass
-
-    # def create(self, request):
-    #     pass
-
-    # def retrieve(self, request, pk=None):
-    #     pass
-
-    # def update(self, request, pk=None):
-    #     pass
-
-    # def partial_update(self, request, pk=None):
-    #     pass
-
-    # def destroy(self, request, pk=None):
-    #     pass
-
-    # class PostList(viewsets.ModelViewSet):
-    #     permission_classes = [IsAuthenticated]
-    #     queryset = Post.postobjects.all()
-    #     serializer_class = PostSerializer
-
-    # class PostDetail(viewsets.ModelViewSet, PostUserWritePermission):
-    #     permission_classes = [PostUserWritePermission]
-    #     queryset = Post.objects.all()
-    #     serializer_class = PostSerializer
-
 """ Concrete View Classes
 # CreateAPIView
 Used for create-only endpoints.
